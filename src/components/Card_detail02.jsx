fe8c02ee73f580b52c30fb92a999b49fbab37f9d
Amandeep Singh <amandeepaluna1@gmail.com>
2025-12-18 16:56:22 +0530

    Add projects

--- src/components/Card_detail02.jsx
+++ src/components/Card_detail02.jsx
@@ -1,15 +1,29 @@
 
 import three from "../images/MyPortfolio4.png"
-
-
+import one from '../images/Scan2Attend.png'
+import two from '../images/pusle.png'
 const cardDataAgain = [
   
   {
     title: "My React Portfolio",
-    description:  "Developed a personal portfolio using React to showcase my projects, skills, and background. The site features a clean, responsive design and smooth navigation to enhance user experienceu",
+    description:  "Developed a personal portfolio using React to showcase my projects, skills, and background. The site features a clean, responsive design and smooth navigation to enhance user experience",
     image: three,
     dribble:"Visit My Portfolio",
     link:"https://portfolio-oo2o.vercel.app/",
+  },
+  {
+    title: "Scan2Attend",
+    description:  "Developed a Scan2Attend application to streamline attendance tracking for teachers and students.",
+    image: one,
+    dribble:"Visit Scan2Attend",
+    link:"https://scan2attend.onrender.com",
+  },
+  {
+    title: "Pulse-Match",
+    description:  "Developed a online platform for connecting people with similar interests.",
+    image: two,
+    dribble:"Visit Pulse-Match",
+    link:"https://pusle-match.onrender.com",
   },
 ];
 
--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -192,7 +192,7 @@ const ChatBot = () => {
 
 
       // no need for this backend code
-      const response = await fetch(`https://generativelanguage.googleapis.com/v1/models/gemini-2.5-pro:generateContent?key=${import.meta.env.VITE_GEMINI_API_KEY}`, {
+      const response = await fetch(`https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=${import.meta.env.VITE_GEMINI_API_KEY}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
